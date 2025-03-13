84c4f86f6d38288d603d0f1eb3f4ce0b76391433
vitormartinsc <vitormartinscarvalho@gmail.com>
2025-03-13 17:40:22 -0300

    New commit

--- equipment_data_analysis.R
+++ equipment_data_analysis.R
@@ -33,14 +33,43 @@ load_packages <- function() {
 
 setwd("C:/Users/servi/inter-scraping")
 load_packages()
-df = read_csv('equipment_data.csv')
+df_inter = read_csv('equipment_data.csv')
 
-df %>% 
+df_inter %>% 
   na.omit %>% 
   select(-1) %>% 
-  distinct(name, equipment_id, .keep_all = T) %>% 
-  group_by(name) %>% 
-  summarise(n = n()) %>% 
-  arrange(-n)
+  distinct(name, equipment_id, .keep_all = T) -> df_inter
+
+df_stock = read_excel('estoque_inter.xlsx')
+
+df_stock %>% 
+  filter(! grepl('Erro', Observação)) %>% 
+  select(1, 3, 4, Status) %>% 
+  select(-Status) %>% 
+  na.omit %>% 
+  purrr::set_names('equipment_id', 'name', 'cpf_cnpj') %>% 
+  na.omit -> df_stock
+
+equipment_ids_raw = df_raw$equipment_id
 
 
+df_stock %>% 
+  mutate(cpf_cnpj = gsub("\\D", "", cpf_cnpj)) %>% 
+  select(equipment_id, cpf_cnpj) %>% 
+  rename(stock_id = 1) -> df_stock_id
+
+df_inter %>% 
+  mutate(cpf_cnpj = gsub("\\D", "", cpf_cnpj)) %>% 
+  select(equipment_id, cpf_cnpj) %>% 
+  rename(inter_id = 1) -> df_inter_id
+
+diff_stock_inter <- df_stock_id %>% 
+  anti_join(df_inter_id, by = c("cpf_cnpj", "stock_id" = "inter_id"))
+
+print(diff_stock_inter)
+
+
+diff_inter_stock <- df_inter_id %>% 
+  anti_join(df_stock_id, by = c("cpf_cnpj", "inter_id" = "stock_id"))
+
+print(diff_inter_stock)
